7a3aaa59be80dcbb7ffed5336aa4e08b7b82d5e6
Irakotyk <93030438+Irakotyk@users.noreply.github.com>
2021-10-25 10:06:40 +0200

    task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
         //String s="name";
         System.out.println("s");
-        //age int="c"
+        //age int="c";
         System.out.println("c");
-       // String d= city
+       // String d=city ;
         System.out.println("d");
 
     }
